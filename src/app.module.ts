dcb68a9b6bd177d6b73e0567ab43da21fd33e92c
phucth <phuctran@dnkinno.com>
2026-03-03 08:40:38 +0700

    Init redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
 
 @Module({
   imports: [
@@ -11,6 +13,13 @@ import { ConfigModule } from '@nestjs/config';
         `.env.${process.env.NODE_ENV || 'development'}`,
         '.env',
       ]
+    }),
+    CacheModule.registerAsync({
+      useFactory: async () => ({
+        stores: [createKeyv('redis://localhost:6379')],
+        ttl: 1000 * 60 * 60,
+      }),
+      isGlobal: true,
     }),
   ],
   controllers: [AppController],
